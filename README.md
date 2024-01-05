7b5dc3233c25eda4dea64c7432fd02dac34eac2d
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-05 09:16:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+User document signature
+------------------------
+This module allows user to it's signature,the signature can be used later in documents.
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'User document signature',
+    'version': '1.0.1',
+    'author': 'Soft-integration',
+    'category': 'Base',
+    'description': "",
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'views/res_users_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
